dfb006b29c1444ba1de3a63b2eaa3b9d4451625f
kafeihaoka <327433126@qq.com>
2020-09-04 14:30:09 +0800

    update code

--- components/basic/index.en-US.md.1fa65f.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{1256:function(e,n){e.exports={content:["article"],meta:{order:0,title:"basic",filename:"components/basic/index.en-US.md"},toc:["ul"]}}}]);
